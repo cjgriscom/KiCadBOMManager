49105fb9a9a0efcf426b0cc804868dd9cb91d44c
Chandler Griscom <cjgriscom@gmail.com>
2021-10-01 13:25:40 -0400

    update

--- src/main/java/io/chandler/kicad/bommanager/BOMExport.java
+++ src/main/java/io/chandler/kicad/bommanager/BOMExport.java
@@ -143,6 +143,8 @@ public class BOMExport {
 		
 		System.out.println("Edit association table? (y/N)");
 		
+		System.out.println("Use /{designator} to search");
+		
 		if (in.nextLine().toLowerCase().startsWith("y")) {
 			System.out.println("Edit part number associations: ");
 			System.out.println();
